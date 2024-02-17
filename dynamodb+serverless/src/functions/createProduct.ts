cd91166638f557388578f46d54eb932248d03547
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-16 21:13:29 -0300

    feat: adding dynamodb client

--- dynamodb+serverless/src/functions/createProduct.ts
+++ dynamodb+serverless/src/functions/createProduct.ts
@@ -1,6 +1,27 @@
-export async function handler() {
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { randomUUID } from "crypto";
+import { dynamoClient } from "../lib/dynamoClient";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  const body = JSON.parse(event.body);
+
+  const id = randomUUID();
+
+  const command = new PutItemCommand({
+    TableName: "ProductsTable",
+    Item: {
+      id: { S: id },
+      name: { S: body.name },
+      price: { N: `${body.price}` },
+      tags: { SS: body.tags },
+    },
+  });
+
+  const response = await dynamoClient.send(command);
+
   return {
     statusCode: 201,
-    body: "Hello World!",
+    body: JSON.stringify(response),
   };
 }
