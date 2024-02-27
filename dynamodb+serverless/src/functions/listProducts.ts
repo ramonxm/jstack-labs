c88c6b8136356cf61c35ef3faab16fd6dcdd96c5
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-26 21:24:43 -0300

    feat: adding scan command

--- dynamodb+serverless/src/functions/listProducts.ts
+++ dynamodb+serverless/src/functions/listProducts.ts
@@ -1,6 +1,15 @@
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { dynamoClient } from "src/lib/dynamoClient";
+
 export async function handler() {
+  const command = new ScanCommand({
+    TableName: "ProductsTable",
+  });
+
+  const { Items } = await dynamoClient.send(command);
+
   return {
     statusCode: 200,
-    body: "Hello World!",
+    body: JSON.stringify(Items),
   };
 }
