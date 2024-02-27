35eef4e76aa579f8366085d343ed6ecfdac4c216
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-26 21:32:45 -0300

    feat: creating get product command

--- dynamodb+serverless/src/functions/getProduct.ts
+++ dynamodb+serverless/src/functions/getProduct.ts
@@ -1,6 +1,28 @@
-export async function handler() {
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { dynamoClient } from "src/lib/dynamoClient";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  const { productId } = event.pathParameters;
+
+  const command = new GetCommand({
+    TableName: "ProductsTable",
+    Key: {
+      id: productId,
+    },
+  });
+
+  const { Item } = await dynamoClient.send(command);
+
+  if (!Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Product not found" }),
+    };
+  }
+
   return {
     statusCode: 200,
-    body: "Hello World!",
+    body: JSON.stringify(Item),
   };
 }
