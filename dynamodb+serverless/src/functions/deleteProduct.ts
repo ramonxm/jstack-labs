5174e5582f07e933fb2a44a68fa0c337cb780740
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-26 21:37:05 -0300

    feat: creating delete command product

--- dynamodb+serverless/src/functions/deleteProduct.ts
+++ dynamodb+serverless/src/functions/deleteProduct.ts
@@ -1,6 +1,20 @@
-export async function handler() {
+import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { dynamoClient } from "src/lib/dynamoClient";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  const { productId } = event.pathParameters;
+
+  const command = new DeleteCommand({
+    TableName: "ProductsTable",
+    Key: {
+      id: productId,
+    },
+  });
+
+  await dynamoClient.send(command);
+
   return {
     statusCode: 204,
-    body: "Hello World!",
   };
 }
