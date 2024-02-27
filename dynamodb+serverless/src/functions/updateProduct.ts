ae1e146058c931d5178d3dea113a39fb61b0c77b
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-26 21:51:47 -0300

    feat: creating update product command

--- dynamodb+serverless/src/functions/updateProduct.ts
+++ dynamodb+serverless/src/functions/updateProduct.ts
@@ -1,6 +1,32 @@
-export async function handler() {
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { dynamoClient } from "src/lib/dynamoClient";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  const body = JSON.parse(event.body);
+  const { productId } = event.pathParameters;
+
+  const command = new UpdateCommand({
+    TableName: "ProductsTable",
+    Key: {
+      id: productId,
+    },
+    UpdateExpression: "set #n :n, #p = :p, #t = :t",
+    ExpressionAttributeNames: {
+      "#n": "name",
+      "#p": "price",
+      "#t": "tags",
+    },
+    ExpressionAttributeValues: {
+      ":n": body.name,
+      ":t": body.tags,
+      ":p": body.price,
+    },
+  });
+
+  await dynamoClient.send(command);
+
   return {
-    statusCode: 200,
-    body: "Updated",
+    statusCode: 204,
   };
 }
