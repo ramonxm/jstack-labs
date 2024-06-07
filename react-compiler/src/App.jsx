06f87c9dd24e5a56ec9650048ecc55b66d2b2bc3
Ramon Xavier <ramon1234rxm@gmail.com>
2024-06-06 23:21:31 -0300

    feat: updating initial page

--- react-compiler/src/App.jsx
+++ react-compiler/src/App.jsx
@@ -1,29 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-import { Logo } from "./Logo";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <Logo />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div> hello</div>;
 }
 
 export default App;
